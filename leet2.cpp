8f848c52b7694f977541886c4252cda0f47dd351
kalyanramgeda <148609878+kalyanramgeda@users.noreply.github.com>
2025-11-25 21:23:22 +0530

    Add files via upload

---
+++ leet2.cpp
@@ -0,0 +1,24 @@
+class solution{
+    public:
+    ListNode* addTwoNumber(ListNode* l1, ListNode* l2){
+        ListNode* dummy = new ListNode(0);
+        listNode* current = dummy;
+        int carry = 0;
+        while(l1||l2||carry){
+            int sum = carry;
+        if(l1){
+            sum += l1->val;
+            l1= l1->next;
+
+        }
+        if(l2){
+            sum += l2->val;
+            l2 = l2->next;
+        }
+        carry = sum / 10;
+        current->next = new ListNode(sum % 10);
+        current - current->next;      
+    }
+    return dummy->next;
+}
+};
